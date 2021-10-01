499788d9c57b1740c53b5fe9f92a3778e47d672e
ahfeng-seas <91761600+ahfeng-seas@users.noreply.github.com>
2021-10-01 17:35:33 -0400

    Add files via upload

--- pos_tagger.py
+++ pos_tagger.py
@@ -1,14 +1,45 @@
 """ Contains the part of speech tagger class. """
-
+import numpy as np
+NUM_TAGS = 36
 def load_data(sentence_file, tag_file=None):
     """Loads data from two files: one containing sentences and one containing tags.
-
+    
     tag_file is optional, so this function can be used to load the test data.
 
     Suggested to split the data by the document-start symbol.
 
     """
-    return []
+    # TODO: Prune punctuation, maybe use dataframes?
+    sentences = []
+    sentence = ['START']
+    sentence_tags = None
+    vocabulary = set()
+    with open(sentence_file, 'r') as x_data:
+        next(x_data) # Skip first -DOCSTART-
+        for line in x_data:
+            word = line.split(',')[1]
+            if word == "-DOCSTART-":
+                sentence.append("END")
+                sentences.append(sentence)
+                sentence = ['START']
+            else: 
+                sentence.append(word)
+                vocabulary.add(word)
+    if(tag_file):
+        sentences_tags = []
+        sentence_tags = ['^']
+        with open(tag_file, 'r') as y_data:
+            next(y_data)
+            for line in y_data:
+                tag = line.split(',')[1]
+                if word == "O":
+                    sentence_tags.append('$')
+                    sentences_tags.append(sentence_tags)
+                    sentence_tags = ['^']
+                else: sentence_tags.append(tag)
+
+
+    return sentences, sentences_tags, vocabulary
 
 def evaluate(data, model):
     """Evaluates the POS model on some sentences and gold tags.
@@ -26,7 +57,12 @@ def evaluate(data, model):
 class POSTagger():
     def __init__(self):
         """Initializes the tagger model parameters and anything else necessary. """
-        pass
+        tag_ids = {'CC':1,'CD':2,'DT':3,'EX':4,'FW':5,'IN':6,'JJ':7,'JJR':8,'JJS':9,'LS':10,'MD':11,'NN':12,\
+            'NNS':13,'NNP':14,'NNPS':15,'PDT':16,'POS':17,'PRP':18,'PRP$':19,'RB':20,'RBR':21,'RBS':22,'RP':23,\
+            'SYM':24,'TO':25,'UH':26,'VB':27,'VBD':28,'VBG':29,'VBN':30,'VBP':31,'VBZ':32,'WDT':33,\
+            'WP':34,'WP$':35,'WRB':36}
+        trigram_transmissions = np.zeros(NUM_TAGS,NUM_TAGS,NUM_TAGS) # q(C|A,B) = t_t[id[A],id[B],id[C]]
+        emissions = np.zeros(vocab_size,NUM_TAGS)
 
     def train(self, data):
         """Trains the model by computing transition and emission probabilities.

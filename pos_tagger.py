60c8932615baeb45f237c13cd1c46df5a9e72513
Xiaoyu Cheng <christinecheng1213@gmail.com>
2021-10-04 21:34:42 -0400

    fixed load function

--- pos_tagger.py
+++ pos_tagger.py
@@ -1,5 +1,9 @@
 """ Contains the part of speech tagger class. """
 import numpy as np
+import string
+import copy
+import sys
+
 NUM_TAGS = 36
 def load_data(sentence_file, tag_file=None):
     """Loads data from two files: one containing sentences and one containing tags.
@@ -9,25 +13,33 @@ def load_data(sentence_file, tag_file=None):
     Suggested to split the data by the document-start symbol.
 
     """
-    # TODO: Prune punctuation, maybe use dataframes?
+
+    #vocabulary = set()
+
     sentences = []
     sentence = ['START']
-    sentence_tags = None
-    vocabulary = set()
+    sentences_tags = []
+    sentence_tags = ['^']
+    ct = 0
+
     with open(sentence_file, 'r') as x_data:
+        print(sentence_file)
         next(x_data) # Skip first -DOCSTART-
         for line in x_data:
-            word = line.split(',')[1]
-            if word == "-DOCSTART-":
+            ct += 1
+            word = line.split(',')[1].replace('"','').strip()
+            if word.strip() == '-DOCSTART-':
                 sentence.append("END")
                 sentences.append(sentence)
                 sentence = ['START']
+            elif word in string.punctuation:
+                continue
             else: 
                 sentence.append(word)
-                vocabulary.add(word)
-    if(tag_file):
-        sentences_tags = []
-        sentence_tags = ['^']
+                #print(line)
+                #vocabulary.add(word)
+    
+    if tag_file:
         with open(tag_file, 'r') as y_data:
             next(y_data)
             for line in y_data:
@@ -36,10 +48,14 @@ def load_data(sentence_file, tag_file=None):
                     sentence_tags.append('$')
                     sentences_tags.append(sentence_tags)
                     sentence_tags = ['^']
-                else: sentence_tags.append(tag)
+                elif word in string.punctuation:
+                    continue
+                else: 
+                    sentence_tags.append(tag)
 
-
-    return sentences, sentences_tags, vocabulary
+    print(sentences)
+    print(sentence_tags)
+    return sentences, sentences_tags
 
 def evaluate(data, model):
     """Evaluates the POS model on some sentences and gold tags.
@@ -61,8 +77,8 @@ class POSTagger():
             'NNS':13,'NNP':14,'NNPS':15,'PDT':16,'POS':17,'PRP':18,'PRP$':19,'RB':20,'RBR':21,'RBS':22,'RP':23,\
             'SYM':24,'TO':25,'UH':26,'VB':27,'VBD':28,'VBG':29,'VBN':30,'VBP':31,'VBZ':32,'WDT':33,\
             'WP':34,'WP$':35,'WRB':36}
-        trigram_transmissions = np.zeros(NUM_TAGS,NUM_TAGS,NUM_TAGS) # q(C|A,B) = t_t[id[A],id[B],id[C]]
-        emissions = np.zeros(vocab_size,NUM_TAGS)
+        #trigram_transmissions = np.zeros(NUM_TAGS,NUM_TAGS,NUM_TAGS) # q(C|A,B) = t_t[id[A],id[B],id[C]]
+        #emissions = np.zeros(vocab_size,NUM_TAGS)
 
     def train(self, data):
         """Trains the model by computing transition and emission probabilities.
@@ -96,22 +112,24 @@ class POSTagger():
 if __name__ == "__main__":
     pos_tagger = POSTagger()
 
-    train_data = load_data("data/train_x.csv", "data/train_y.csv")
-    dev_data = load_data("data/dev_x.csv", "data/dev_y.csv")
-    test_data = load_data("data/test_x.csv")
+    train_data, train_tags = load_data("data/train_x.csv", "data/train_y.csv")
+    #dev_data, dev_tags = load_data("data/dev_x.csv", "data/dev_y.csv")
+    #test_data, test_tags = load_data("data/test_x.csv")
 
-    pos_tagger.train(train_data)
+    print(train_data)
+    print(train_tags)
+    #pos_tagger.train(train_data)
 
     # Experiment with your decoder using greedy decoding, beam search, viterbi...
 
     # Here you can also implement experiments that compare different styles of decoding,
     # smoothing, n-grams, etc.
-    evaluate(dev_data, pos_tagger)
+    #evaluate(dev_data, pos_tagger)
 
     # Predict tags for the test set
-    test_predictions = []
-    for sentence in test_data:
-        test_predictions.extend(pos_tagger.inference(sentence)
+    # test_predictions = []
+    # for sentence in test_data:
+    #     test_predictions.extend(pos_tagger.inference(sentence)
     
     # Write them to a file to update the leaderboard
     # TODO

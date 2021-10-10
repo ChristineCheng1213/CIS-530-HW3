ee5cca68e30b4616367a6a45789b8387d82e36c1
ahfeng-seas <91761600+ahfeng-seas@users.noreply.github.com>
2021-10-09 20:44:41 -0400

    viterbi (untested)

--- pos_tagger.py
+++ pos_tagger.py
@@ -11,6 +11,7 @@ TAGS = {'START':0,'CC':1,'CD':2,'DT':3,'EX':4,'FW':5,'IN':6,'JJ':7,'JJR':8,'JJS'
     'NNS':13,'NNP':14,'NNPS':15,'PDT':16,'POS':17,'PRP':18,'PRP$':19,'RB':20,'RBR':21,'RBS':22,'RP':23,\
     'SYM':24,'TO':25,'UH':26,'VB':27,'VBD':28,'VBG':29,'VBN':30,'VBP':31,'VBZ':32,'WDT':33,\
     'WP':34,'WP$':35,'WRB':36, 'END':37}
+TAG_IDS = {v:k for k,v in TAGS.items()}
 NUM_TAGS = len(TAGS)
 
 
@@ -258,7 +259,7 @@ class POSTagger():
     """
     def get_emissions(self, data):
         self.word_encodings = self.encode_words(data[0])
-        unigram = ngram_counter(data[0], data[1], 1)
+        unigram = self.ngram_counter(data[0], data[1], 1)
         tag_assignment = self.tag_assignment_counter(data[0], data[1])
         num_words = len(self.word_encodings)
         emission_matrix = np.zeros((num_words, NUM_TAGS))
@@ -313,6 +314,46 @@ class POSTagger():
         """Generates tags through Viterbi algorithm
         
         """
+        lattice = np.zeros((NUM_TAGS**2,len(sequence)))
+        backpointers = np.zeros((NUM_TAGS**2, len(sequence)))
+        lattice[0,0] = 1 #u,v,k -> NUM_TAGS*u+v,k
+        nonzero_indices = 0 #keep track of the nonzero pi values from the previous time step
+        for k in range(1,len(sequence)):
+            maxes = {[] for i in NUM_TAGS } #highest pi values and path for each tag
+            for index in nonzero_indices:
+                u = index % NUM_TAGS
+                v = index // NUM_TAGS
+                prev_pi = lattice[index,k-1]
+                i_max = 0 
+                max_tag = -1
+                for i in NUM_TAGS:
+                    pi = prev_pi * self.trigram_transmissions[u][v][i] * self.emissions[sequence[k]][i]
+                    if pi > i_max:
+                        i_max = pi 
+                        max_tag = i
+                maxes[max_tag].append((index,i_max))
+            for i in NUM_TAGS:
+                best_path = max(maxes[i], key=lambda x: x[1])
+                v = best_path[0] // NUM_TAGS
+                lattice[v*NUM_TAGS+i, k] = best_path[1]
+                backpointers[v*NUM_TAGS+i, k] = best_path[0]
+        endpoints = []
+        for i in range(NUM_TAGS-1,NUM_TAGS**2-1,NUM_TAGS):
+            endpoints.append((i,lattice[i,len(sequence)-1]))
+        best_endpoint = max(endpoints, key= lambda x: x[1])
+        tags = [best_endpoint[0]//NUM_TAGS,NUM_TAGS-1] # Initializes tags with end tag and best preceding tag
+        for k in range(len(sequence)-2,-1,-1):
+            prev_index = backpointers[tags[0]*NUM_TAGS+tags[1],k]
+            tags.insert(0,prev_index//NUM_TAGS)
+        return [TAG_IDS[tag] for tag in tags]
+
+
+            
+
+
+
+
+
 
 
 
